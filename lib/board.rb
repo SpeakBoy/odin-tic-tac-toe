da06ba90350d989da6273b0b6dd2b9387e9099f0
SpeakBoy <speakman@mcmaster.ca>
2024-07-14 13:44:06 -0400

    Add get element method

--- lib/board.rb
+++ lib/board.rb
@@ -13,6 +13,10 @@ class Board
     def update_element(row_index, column_index, char)
         @board[row_index - 1][column_index - 1] = char
     end
+
+    def get_element(row_index, column_index)
+        @board[row_index - 1][column_index - 1]
+    end
 end
 
 test = Board.new([[" "," "," "],[" "," "," "],[" "," "," "]])
@@ -21,3 +25,5 @@ test.update_element(2,2,"X")
 test.print_board
 test.update_element(3,1,"O")
 test.print_board
+puts test.get_element(2,2)
+puts test.get_element(1,1)
