c816da1567a37cc49ac763739c3c55b54de83cde
SpeakBoy <speakman@mcmaster.ca>
2024-07-14 13:47:36 -0400

    Remove lines used for testing

--- lib/board.rb
+++ lib/board.rb
@@ -18,12 +18,3 @@ class Board
         @board[row_index - 1][column_index - 1]
     end
 end
-
-test = Board.new([[" "," "," "],[" "," "," "],[" "," "," "]])
-test.print_board
-test.update_element(2,2,"X")
-test.print_board
-test.update_element(3,1,"O")
-test.print_board
-puts test.get_element(2,2)
-puts test.get_element(1,1)
